3c1a08d81244452fc27dca86d0e6d2bcebf24ce3
vincecfl <vincecfl@gmail.com>
2021-02-20 10:31:43 +0800

    update wap page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
-import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:flash_web/page/pc/swap_pc_page.dart';
 import 'package:flash_web/page/wap/swap_wap_page.dart';
 import 'package:flash_web/provider/index_provider.dart';
+import 'package:fluro/fluro.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:fluro/fluro.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 
@@ -27,54 +26,50 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     final isNotMobile = !PlatformDetector().isMobile();
-    return DynamicTheme(
-      defaultBrightness: Brightness.light,
-      data: (brightness) {
-        return ThemeData(
-          primarySwatch: Colors.blueGrey,
-          backgroundColor: Colors.white,
-          cardColor: Colors.white,
-          primaryTextTheme: TextTheme(
-            button: TextStyle(
-              color: Colors.blueGrey,
-              decorationColor: Colors.blueGrey[300],
-            ),
-            subtitle2: TextStyle(
-              color: Colors.blueGrey[900],
-            ),
-            subtitle1: TextStyle(
-              color: Colors.black,
-            ),
-            headline1: TextStyle(color: Colors.blueGrey[800]),
-          ),
-          bottomAppBarColor: Colors.blueGrey[800],
-          iconTheme: IconThemeData(color: Colors.blueGrey),
-          brightness: brightness,
-        );
+    return MaterialApp(
+      title: 'Flash Finance',
+      theme: MyTheme.lightTheme,
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: Application.router.generator,
+      home: isNotMobile ? SwapPcPage() : SwapWapPage(),
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      localeListResolutionCallback: (locales, supportedLocales) {
+        print(locales);
+        return;
       },
-      themedWidgetBuilder: (context, data) => MaterialApp(
-        title: 'Flash Finance',
-        theme: data,
-        debugShowCheckedModeBanner: false,
-        onGenerateRoute: Application.router.generator,
-        home: isNotMobile ? SwapPcPage() : SwapWapPage(),
-        localizationsDelegates: [
-          S.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-        ],
-        supportedLocales: S.delegate.supportedLocales,
-        localeListResolutionCallback: (locales, supportedLocales) {
-          print(locales);
-          return;
-        },
-      ),
     );
   }
+}
+
+class MyTheme {
+  static final ThemeData lightTheme = ThemeData(
+    primarySwatch: Colors.blueGrey,
+    backgroundColor: Colors.white,
+    cardColor: Colors.white,
+    primaryTextTheme: TextTheme(
+      button: TextStyle(
+        color: Colors.blueGrey,
+        decorationColor: Colors.blueGrey[300],
+      ),
+      subtitle2: TextStyle(
+        color: Colors.blueGrey[900],
+      ),
+      subtitle1: TextStyle(
+        color: Colors.black,
+      ),
+      headline1: TextStyle(color: Colors.blueGrey[800]),
+    ),
+    bottomAppBarColor: Colors.blueGrey[800],
+    iconTheme: IconThemeData(color: Colors.blueGrey),
+    brightness: Brightness.light,
+  );
 }
